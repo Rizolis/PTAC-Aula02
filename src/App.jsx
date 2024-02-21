f04e559c9174e850226c527af75f219c0d3a4174
Rizolis <vitor.correa@estudante.ifms.edu.br>
2024-02-21 15:57:30 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ export default function App() {
 
   const nome = "Rizolis";
   
+  const nome2 = "ainnnn nobru"
   return(
     <div>
         <h1>Hello Moto</h1>
         <p>Desenvolvido por: {nome}</p>
+        <p>ainnnn cachorro </p>
     </div>  
   );
-  <div>
-  <p>um dia eu vi um cahorro </p>
-</div>
-
+  
   }
