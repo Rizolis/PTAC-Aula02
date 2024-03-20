5e74b1958fed11eceedf7e32fbaaa5a1899f101a
Rizolis <vitor.correa@estudante.ifms.edu.br>
2024-03-20 16:08:53 -0400

    aula 20/03

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header(){
     return(
         <header>
-          <h1>Header</h1> 
+          <h1>Cotatinhos do zip zap</h1> 
         </header>
     )
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+export default function Main(){
+
+    const [nome, setNome] = useState("");
+    const [telefone, setTelefone] = useState("");
+
+    
+    return(
+        <main>
+            <form>
+                <label htmlFor="nome">Nome:</label>
+           <input
+           type="text"
+           name=""
+           id=""
+           onChange={(event)=> setNome(event.target.value)}
+           />
+           {nome}
+<br>
+</br>
+<br>
+</br>
+           <label htmlFor="telefone">Telefone:</label>
+           <input
+           type=""
+           name=""
+           id=""
+            onChange={(event) => setTelefone(event.target.value)}
+            />
+            {telefone}
+<br>
+</br>
+<br>
+</br>
+        <button type="submit">Salvar</button> 
+        </form>
+        </main>
+    );
+
+}
